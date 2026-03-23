97d31110c1c25a881419954c929a24bc9e0335a0
ssnehaxxash <snehasunilpathak@gmail.com>
2026-03-23 09:57:31 +0530

    uc4:  Maintain Ordered Bogie IDs

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,21 +1,28 @@
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=== Train Consist Management App ===");
-
+        Scanner sc = new Scanner(System.in);
         Set<String> bogieIds = new HashSet<>();
 
-        bogieIds.add("B101");
-        bogieIds.add("B102");
-        bogieIds.add("B103");
+        System.out.println("=== Train Consist Management App ===");
 
-        bogieIds.add("B102");
-        bogieIds.add("B101");
+        System.out.print("Enter number of bogie IDs: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter bogie ID: ");
+            String id = sc.nextLine();
+            bogieIds.add(id);
+        }
 
         System.out.println("Unique Bogie IDs: " + bogieIds);
+
+        sc.close();
     }
 }
